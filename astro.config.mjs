ff5244d5b4a759b0afe2c724863b164cc3d5930a
Jorge-Bus-11 <jorgebus1111@gmail.com>
2025-10-22 11:13:33 +0100

    ariston

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://jorge-bus-11.github.io'
+  site: 'https://jorge-bus-11.github.io',
+  base: 'portafolio'
 })
